688c543c0321355162e88c73eef224e5f3ab1353
off94 <33528745+off94@users.noreply.github.com>
2018-06-04 00:52:59 +0200

    Update propia3-1.sql

--- Practica 3/propia3-1.sql
+++ Practica 3/propia3-1.sql
@@ -1,8 +1,8 @@
---Empresa creadora del modelo de avion mas usado
+--Empresas creadoras del top 3 de modelos de avion mas usados
 
 SELECT Count(empresa), empresa	--parejas de avion modelo
 FROM Avion JOIN Modelo		--se miran las empresas y se cuentan
 ON Avion.modelo = Modelo.id
-WHERE rownum = 1
+WHERE rownum <= 3
 GROUP BY empresa
 ORDER BY Count(empresa) DESC
