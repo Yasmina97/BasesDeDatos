7b506dec2e9b94bacff0f3807dd46a5f405c351d
IsraelGim <38373607+IsraelGim@users.noreply.github.com>
2018-06-19 20:40:32 +0200

    Update Trigger3.sql

--- Practica 3/Trigger3.sql
+++ Practica 3/Trigger3.sql
@@ -1,6 +1,6 @@
 --Controla que la fecha de un avion tenga sentido, no anteriores a 1946, cuando se producen los primeros vuelos comerciales
 CREATE OR REPLACE TRIGGER tercero
-	BEFORE INSERT OR UPDATE ON ModeloAvion
+	BEFORE INSERT OR UPDATE ON Modelo
 	FOR EACH ROW
 BEGIN
 	
