20c2681fd6e08f65c2cb533f4ba5d2723b4611d6
IsraelGim <38373607+IsraelGim@users.noreply.github.com>
2018-06-19 20:24:48 +0200

    Delete Consulta3.sql

--- Practica 3/Consulta3.sql
+++
@@ -1,8 +0,0 @@
---Porcentaje de vuelos de cada compañía que despegan y aterrizan en el mismo estado
-
-CREATE VIEW mismoEstado AS SELECT COUNT(ae.estado) FROM Aeropuerto AS ae JOIN Vuelo AS v WHERE
-(ae.aeroOrigen, ae.estado)==(ae.aeroDestino, ae.estado)  --mira los vuelo sque tengan el mismo estado en los aeropuertos origen y destino
-
-CREATE VIEW total AS SELECT COUNT(ae.estado) FROM Aeropuerto AS ae --mira los vuelos totales
-
-SELECT mismoEstado*100/total FROM Aeropuerto, Vuelo
