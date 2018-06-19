00e5b548450ad74be93ff2c6410c5f8be0fea901
IsraelGim <38373607+IsraelGim@users.noreply.github.com>
2018-06-19 20:44:43 +0200

    Update Trigger1.sql

--- Practica 3/Trigger1.sql
+++ Practica 3/Trigger1.sql
@@ -1,13 +1,13 @@
 --Controla que no se inserte un avion en el momento en el que está en vuelo
 CREATE OR REPLACE TRIGGER primero
-before INSERT OR UPDATE ON planV
+before INSERT OR UPDATE ON Vuelo
 FOR EACH ROW
 DECLARE
     new NUMBER;
 BEGIN
-	select count(*) into new from ejecuccion where fecha=:New.fecha AND idavion=:NEW.idavion 
-										and ((horaSalida>:New.horaSalida and horaSalida<:New.horaLlegada)
-										or (horaLlegada>:New.horaSalida and horaLlegada<:New.horaLlegada));
+	select count(*) into new from vuelo where fecha=:New.fecha AND id=:NEW.id 
+										and ((Salida>:New.Salida and Salida<:New.Llegada)
+										or (Llegada>:New.Salida and Llegada<:New.Llegada));
 	if (new != 0) then
 			RAISE_APPLICATION_ERROR (-20000, 'El avion esta en vuelo');
 	end if;
