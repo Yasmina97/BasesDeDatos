1132a42e2b8d38185d2314ea74e2f3322c90a53d
IsraelGim <38373607+IsraelGim@users.noreply.github.com>
2018-06-19 20:34:21 +0200

    Update Consulta1.sql

--- Practica 3/Consulta1.sql
+++ Practica 3/Consulta1.sql
@@ -1,21 +1,21 @@
--- Compañias que han operado al menos 1000 vuelos cada día.
+-- CompaÃ±ias que han operado al menos 1000 vuelos cada dÃ­a.
 
-SELECT v.compañia
+SELECT v.companyia
 FROM vuelo as v
-GROUP BY v.fecha, v.compañia
+GROUP BY v.fecha, v.companyia
 HAVING count(*) >= 1000
 
--- Compañías aéreas con su media de retrasos de salida y de llegada,
+-- CompaÃ±Ã­as aÃ©reas con su media de retrasos de salida y de llegada,
 -- ordenados por retraso de salida, siempre que hayan operado al menos
--- 1000 vuelos cada día.
+-- 1000 vuelos cada dÃ­a.
 
-SELECT v.compañia, avg(i.retraso) as media
+SELECT v.companyia, avg(i.retraso) as media
 FROM vuelo as v join incidencia as i on v.id=i.vueloAfectado
-WHERE v.compañia IN (
-	SELECT v.compañia
+WHERE v.companyia IN (
+	SELECT v.companyia
 	FROM vuelo as v
-	GROUP BY v.fecha, v.compañia
+	GROUP BY v.fecha, v.companyia
 	HAVING count(*) >= 1000
 )
-GROUP BY v.compañia
+GROUP BY v.companyia
 ODER BY media
